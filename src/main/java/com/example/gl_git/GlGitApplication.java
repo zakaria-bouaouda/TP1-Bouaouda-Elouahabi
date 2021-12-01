b1a5288312f78a2446d5d9af08ec58ec4a087500
reda-coder <redaelouahabi98@gmail.com>
2021-12-01 11:16:41 +0100

    4-commit changes to local Repo (git)

---
+++ src/main/java/com/example/gl_git/GlGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.gl_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlGitApplication.class, args);
+	}
+
+}
